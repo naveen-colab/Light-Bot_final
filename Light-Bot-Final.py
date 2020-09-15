41c4a3180095e86bc2caa6c9f3c33accd2db0eb9
tarunsridhara <67376460+tarunsridhara@users.noreply.github.com>
2020-09-15 23:44:40 +0530

    Update Light-Bot-Final.py

--- Light-Bot-Final.py
+++ Light-Bot-Final.py
@@ -6,53 +6,47 @@
 #adafruit part
 X = os.getenv('X')                     # ADAFRUIT_IO_USERNAME
 Y = os.getenv('Y')                     # ADAFRUIT_IO_KEY
-
 from Adafruit_IO import Client, Feed
 aio = Client(X,Y)
 
 
+#logging exception handler
+import logging
+import os
 
-#telegram part
-from telegram.ext import Updater,CommandHandler, MessageHandler, Filters  
-
-import requests  # Getting the data from the cloud
-
-def get_url():
-    contents = requests.get('https://random.dog/woof.json').json()
-    url = contents ['url']
-    return url
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-def lighton(bot,update):
-    data = aio.send('lightbot', 1)
-    rdata = aio.receive('lightbot').value
-    chat_id = update.message.chat_id
-    bot.send_message(chat_id,text='Light is On')
-    url = get_url()
-    bot.send_photo(chat_id,photo=url)
-    bot.send_message(chat_id,text='Request processed')
 
+
+from telegram.ext import Updater, CommandHandler,MessageHandler, Filters 
 
 def lightoff(bot,update):
     data = aio.send('lightbot', 0)
     rdata = aio.receive('lightbot').value
-    chat_id = update.message.chat_id
-    bot.send_message(chat_id,text='Light is Off')
-    url = get_url()
-    bot.send_photo(chat_id,photo=url)
-    bot.send_message(chat_id,text='Request processed')
-
-
+    chat_id = bot.message.chat_id
+    bot.message.reply_text('Request processing')
+    update.bot.sendPhoto(chat_id=chat_id, photo="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ8jpdHR_pFaoP2Vudl7k-46mSK7I3shuCvFw&usqp=CAU", caption="Light off")
+    
+def lighton(bot,update):
+    data = aio.send('lightbot', 1)
+    rdata = aio.receive('lightbot').value
+    chat_id = bot.message.chat_id
+    bot.message.reply_text('Request Processing')
+    update.bot.sendPhoto(chat_id=chat_id, photo="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRgKuBghXuR_IjPXnRu9o2znn0O_avidTs-ig&usqp=CAU", caption="Light on")
+    
 def chooser(bot,update):
-          chat_id = update.message.chat_id
+          chat_id = bot.message.chat_id
             
-          a = update.message.text
+          a = bot.message.text
 
           data = aio.receive_previous('lightbot')
 
           if a == "Light on" or a =="Light ON" or a =="Light On" or a == "LIGHT ON" or a =="light on" and data == 0:
                 { 
-                        lighton(bot,update)
+                       lighton(bot,update)
                 }
           elif a == "Light off" or a =="Light OFF" or a =="Light Off" or a == "LIGHT OFF" or a =="light off" and data == 1:
                 {
@@ -63,10 +57,16 @@ def chooser(bot,update):
                         bot.send_message(chat_id,text='Invalid Text')
                 }
 
+                
+                
+def main():
+  BOT_TOKEN= '1359570647:AAGqrbWWBWa2DDn4uKD39UHACCzDHPMNYpo'
+  u = Updater(BOT_TOKEN, use_context=True)
+  dp = u.dispatcher
+  dp.add_handler(MessageHandler(Filters.text, chooser))
+  u.start_polling()
+  u.idle()
 
-
-u = Updater('1171508830:AAHdsf3BzMRpIHVpGOdh5XGFYChRSTNGwOo')
-dp = u.dispatcher
-dp.add_handler(MessageHandler(Filters.text, chooser))
-u.start_polling()
-u.idle()
+    
+if __name__ == '__main__':
+    main()
